0d951ded31f077e652e7ca9874a711a9670d46ba
Stefanie <stefcodes@gmail.com>
2023-01-07 22:18:14 -0700

    add nfts to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import { ConnectWallet, useActiveListings, useContract } from "@thirdweb-dev/react";
+import { ConnectWallet, MediaRenderer, useActiveListings, useContract } from "@thirdweb-dev/react";
+import { BigNumber } from "ethers";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
@@ -12,15 +13,33 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">SoundPhase</a>!
+          Welcome to <a href="http://thirdweb.com/">SoundPhase</a>
         </h1>
         <ConnectWallet/>
         {!isLoading ? (
           <div>
             {nfts && nfts.map((nft) => {
               return (
-                <div>{nft.asset.name}</div>
-              )
+                <div>
+                  <MediaRenderer
+                  src={nft.asset.image}
+                  height="200px"
+                  width="200px"
+                  />
+                  <p>{nft.asset.name}</p>
+                  <p>Price: {nft.buyoutCurrencyValuePerToken.displayValue} MATIC</p>
+                  <button
+                    onClick={async () => {
+                      try {
+                        await contract?.buyoutListing(BigNumber.from(nft.id), 1); 
+                      } catch (error) {
+                        console.error(error); 
+                        alert(error); 
+                      }
+                    }}
+                  > Buy Now</button>
+                </div>
+              );
             })}
           </div>
         ) : (
